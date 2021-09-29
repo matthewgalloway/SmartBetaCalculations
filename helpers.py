90b08b269f85c8b4f7153795fd4d97de0ddc875c
mgalloway <matthew.galloway19@gmail.com>
2021-09-29 09:11:25 +0100

    queries to api added

--- helpers.py
+++ helpers.py
@@ -1,7 +1,8 @@
 import config
 from helpers import *
 import pandas as pd
-
+from api_setup import make_api_call
+import time
 
 def reset_header(df):
 #   changes the first row of the df to the column names
@@ -17,3 +18,20 @@ def get_stocks_from_wiki(url):
     stocks_df = get_html_table(url)
     stocks_df = reset_header(stocks_df)
     return stocks_df
+
+def get_stock_price(url):
+    stocks_df = get_stocks_from_wiki(url)
+    stocks_list = list(set(stocks_df.Symbol))
+
+    snp_components_timeseries = pd.DataFrame()
+    failed_symbols =[]
+
+    for symbol in stocks_list:
+        try:
+            time.sleep(1)
+            component_data = make_api_call(symbol)
+            snp_components_timeseries = snp_components_timeseries.append(component_data)
+        except:
+            failed_symbols.append(symbol)
+            
+    return snp_components_timeseries
